f0c53efac985ce7b4f0fdc69e76616403a2f8937
hannah <maicamaningo440@gmail.com>
2026-03-25 18:08:34 +0800

    Redesign login page styling

--- StudentViolationWeb/Model/Login.cs
+++ StudentViolationWeb/Model/Login.cs
@@ -1,17 +1,17 @@
-﻿//namespace StudentViolationWeb.Model
-//{
-   
-//        public class Login
-//        {
-//            public string Username { get; set; } = "";
-//            public string Password { get; set; } = "";
-//        }
+﻿namespace StudentViolationWeb.Model
+{
 
-//        public class LoginResponse
-//        {
-//            public bool Success { get; set; }
-//            public string Message { get; set; } = "";
-//            public string Role { get; set; } = "";
-//            public string Token { get; set; } = "";
-//        }
-//    }
+    public class Login
+    {
+        public string Username { get; set; } = "";
+        public string Password { get; set; } = "";
+    }
+
+    public class LoginResponse
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; } = "";
+        public string Role { get; set; } = "";
+        public string Token { get; set; } = "";
+    }
+}
