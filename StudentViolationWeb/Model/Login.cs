5b7f6c8c8163662e43b2b37d5b40373170b6c382
hannah <maicamaningo440@gmail.com>
2026-03-27 22:21:14 +0800

    redesign register page

--- StudentViolationWeb/Model/Login.cs
+++ StudentViolationWeb/Model/Login.cs
@@ -3,15 +3,15 @@
 
     public class Login
     {
-        public string Username { get; set; } = "";
-        public string Password { get; set; } = "";
+        public string Username { get; set; } = " ";
+        public string Password { get; set; } = " ";
     }
 
     public class LoginResponse
     {
-        public bool Success { get; set; }
-        public string Message { get; set; } = "";
-        public string Role { get; set; } = "";
-        public string Token { get; set; } = "";
+        public bool Success { get; set; }  
+        public string Message { get; set; }= " ";
+        public string Role { get; set; }   = " ";
+        public string Token { get; set; } = " ";
     }
 }
